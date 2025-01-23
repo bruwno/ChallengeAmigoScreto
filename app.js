ee9dbd66e63bf9022868d8d75dff6788f8de1c3a
William Silva <williamsilvajdf@gmail.com>
2025-01-23 20:57:53 -0300

    Agregando amigos
    Implementando a função de adicionar amigos à lista juntamente com a validação do dado a ser inserido na lista.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function validarEntrada(nome) {
+    return (nome.length) === 0 ? "" : nome;
+}
+
+function adicionarAmigo() {
+    let campoNomeAmigo = document.getElementById("amigo");
+    let nomeAmigo = validarEntrada(campoNomeAmigo).value;
+
+    if (nomeAmigo !== "") {
+        adicionarNaLista(nomeAmigo);
+        campoNomeAmigo.value = "";
+    } else {
+        alert("Informe um nome válido!");
+    }
+}
+
+function adicionarNaLista(nome) { 
+    listaAmigos.push(nome);
+}
